babe34cfc1f5716587b9ef31eda1519013a57fd9
Amogh Ghadge <amoghghadge22@gmail.com>
2026-03-22 17:59:20 -0400

    initializing android and ios apps

---
+++ CartQuestiOS/CartQuestiOS/CartQuestiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartQuestiOSApp.swift
+//  CartQuestiOS
+//
+//  Created by Amogh Ghadge on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct CartQuestiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
